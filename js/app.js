44eb1fb8af1a589bfca657c9e54bba3336a2e4b8
AriannaCF <106922479+AriannaCF@users.noreply.github.com>
2022-09-03 11:38:18 -0400

    docs: refine javascript
    Update js file, add for each loops, slight adjustments to css file

--- js/app.js
+++ js/app.js
@@ -18,21 +18,11 @@
  * Great to have comments before crucial code sections within the procedure.
 */
 
-/**
- * Define Global Variables
- * 
-*/
-
-//navbar variables
+/* Global Variables */
 const navbar = document.querySelector('#navbar');
 const menu = document.querySelector('#hamburger-menu');
 const sections = document.querySelectorAll('.division');
-const resourcesSection = document.querySelector('#section1');
-const educationSection = document.querySelector('#section2');
-const designSection = document.querySelector('#section3');
-const projectSection = document.querySelector('#section4');
 
-//viewport variables
 const viewportHeight = document.documentElement.clientHeight;
 const subsections = document.querySelectorAll('.subdivision');
 
@@ -44,9 +34,7 @@ const subsections = document.querySelectorAll('.subdivision');
 */
 
 function highlighter() {
-    //loop through subsections
-    for (subsection of subsections) {
-        //find subsection position
+    subsections.forEach(subsection => {
         const top = subsection.getBoundingClientRect().top;
         const bottom = subsection.getBoundingClientRect().bottom;
         //if subsection is in viewport, add class, remove once subsection is out of viewport
@@ -56,6 +44,7 @@ function highlighter() {
             subsection.classList.remove('active');
         }
     }
+    )
 }
 
 function navBarFunctionality () {
@@ -68,15 +57,17 @@ function navBarFunctionality () {
 }
 
 document.addEventListener ('scroll', highlighter);
-menu.addEventListener('click', navBarFunctionality)
+menu.addEventListener('click', navBarFunctionality);
 
 
-// Build menu 
-for (section of sections) {
+
+// Build menu \
+
+sections.forEach(section => {
     const navItem = document.createElement('li');
     navbar.append(navItem);
     const anchor = document.createElement('a');
     anchor.setAttribute('href',`#${section.dataset.section}`);
     anchor.innerText=section.dataset.nav;
     navItem.append(anchor);
-}
+});
