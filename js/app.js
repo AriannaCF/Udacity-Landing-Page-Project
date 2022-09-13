6614f8b66a3018fcf10d0fe45961dcb48b04cf5b
AriannaCF <106922479+AriannaCF@users.noreply.github.com>
2022-09-13 11:05:05 -0400

    docs: revisions
    Add active states for nav items and active section in viewport. Impliment smooth scroll with javascript. Add back to top button that appears below the fold.

--- js/app.js
+++ js/app.js
@@ -25,7 +25,8 @@ const sections = document.querySelectorAll('.division');
 
 const viewportHeight = document.documentElement.clientHeight;
 const subsections = document.querySelectorAll('.subdivision');
-
+const up = document.querySelector('#up-chevron');
+const header = document.querySelector('#page-header');
 
 /**
  * End Global Variables
@@ -33,11 +34,11 @@ const subsections = document.querySelectorAll('.subdivision');
  * 
 */
 
+//activates styling when element is in the viewport
 function highlighter() {
     subsections.forEach(subsection => {
         const top = subsection.getBoundingClientRect().top;
         const bottom = subsection.getBoundingClientRect().bottom;
-        //if subsection is in viewport, add class, remove once subsection is out of viewport
         if (top<viewportHeight && bottom>0) {
             subsection.classList.add('active');
         } else {
@@ -46,21 +47,46 @@ function highlighter() {
     }
     )
 }
-
+//highlights active section in navbar
+function classMaster () {
+    let current = navbar.getElementsByClassName('active');
+    if (current.length>0) {
+        current[0].className = current[0].className.replace(' active', '');
+    }
+    this.className+=' active';
+}
+//toggles hamburger menu on smaller screens
 function navBarFunctionality () {
-    //toggle navbar display on small screens
     if (navbar.style.display==="none") {
         navbar.style.display="block";
     } else {
         navbar.style.display="none";
     }
 }
-
+//scroll back to top
+function backToTop (e) {
+    e.preventDefault();
+    header.scrollIntoView({
+        behavior:'smooth'
+    })
+}
+//hide up chevron above the fold
+function scrollUp () {
+    let y = window.scrollY;
+    if (y>=812) {
+        up.classList.add('shown');
+        up.classList.remove('hidden');
+    } else { 
+        up.classList.remove('shown');
+        up.classList.add('hidden')
+    }
+}
 //Add event listeners
 
 document.addEventListener ('scroll', highlighter);
+document.addEventListener ('scroll', scrollUp)
 menu.addEventListener('click', navBarFunctionality);
-
+up.addEventListener('click', backToTop);
 
 
 // Build menu
@@ -69,7 +95,18 @@ sections.forEach(section => {
     const navItem = document.createElement('li');
     navbar.append(navItem);
     const anchor = document.createElement('a');
-    anchor.setAttribute('href',`#${section.dataset.section}`);
+    anchor.setAttribute('class', 'button');
     anchor.innerText=section.dataset.nav;
     navItem.append(anchor);
-});
+//smooth scroll to clicked link
+    navItem.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(`#${section.dataset.section}`).scrollIntoView({
+            behavior:'smooth'
+        });
+    })
+    const segments = document.getElementsByClassName('button');
+    for (let i=0; i<segments.length; i++) {
+        segments[i].addEventListener('click', classMaster)
+        }});
+
